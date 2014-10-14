acdcf426a32c66f85139f6756a5cafd4d72e8063
Julio Arias <jarias01@gmail.com>
2014-10-13 19:43:27 -0600

    improve setup script

--- mac-setup.sh
+++ mac-setup.sh
@@ -3,9 +3,15 @@
 #No password sudo
 sudo sed -i .bak -e 's/^# \(%wheel.*ALL=(ALL) NOPASSWD: ALL\)/\1/g' /etc/sudoers
 sudo dscl . append /Groups/wheel GroupMembership $USER
+
+#Install XCode Command-Line tools
+sudo ./install-xcode.sh
+
 #Install Ansible
 git clone https://github.com/ansible/ansible.git ~/src/github.com/ansible/ansible; cd ~/src/github.com/ansible/ansible; sudo make install
+
 #Run Ansible
 PYTHONIOENCODING='utf-8' HOMEBREW_CASK_OPTS="--appdir=/Applications" ansible-galaxy install -r requirements.yml && ansible-playbook desktop.yml
+
 #Return to normal password sudo
 sudo dscl . delete /Groups/wheel GroupMembership $USER
