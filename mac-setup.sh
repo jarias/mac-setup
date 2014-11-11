a2bf41a8119a67477c04d7f1bb3306d12059b8a3
Julio Arias <jarias01@gmail.com>
2014-11-11 08:23:23 -0600

    Remove external dependencies of ansible packages

--- mac-setup.sh
+++ mac-setup.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ ! -f /usr/local/bin/brew ]; then
+	#Install homebrew
+	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
 #No password sudo
 sudo sed -i .bak -e 's/^# \(%wheel.*ALL=(ALL) NOPASSWD: ALL\)/\1/g' /etc/sudoers
 sudo dscl . append /Groups/wheel GroupMembership $USER
@@ -9,8 +14,7 @@ sudo easy_install pip
 sudo CFLAGS=-Qunused-arguments CPPFLAGS=-Qunused-arguments pip install ansible
 
 #Run Ansible
-rm -rf roles/osxc.common-env roles/osxc.packages roles/osxc.repository
-PYTHONIOENCODING='utf-8' HOMEBREW_CASK_OPTS="--appdir=/Applications" ansible-galaxy install -r requirements.yml && ansible-playbook desktop.yml
+PYTHONIOENCODING='utf-8'  ansible-playbook desktop.yml
 
 #Return to normal password sudo
 sudo dscl . delete /Groups/wheel GroupMembership $USER
