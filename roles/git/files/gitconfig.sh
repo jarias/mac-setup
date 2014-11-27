b8c9c6d70023652ede8ffb5be3ab8289e0a2292b
Julio Arias <jarias01@gmail.com>
2014-11-27 15:14:14 -0600

    Don't use my edify email account as default git email address

--- roles/git/files/gitconfig.sh
+++ roles/git/files/gitconfig.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 git config --global user.name "$(finger `whoami` | awk -F: '{ print $3 }' | head -n1 | sed 's/^ //')"
-git config --global user.email "$(finger `whoami` | awk -F: '{ print $3 }' | head -n1 | sed 's/^ //' | sed 's/ /\./' | awk '{print tolower($0)"@edify.cr"}')"
+git config --global user.email jarias01@gmail.com
