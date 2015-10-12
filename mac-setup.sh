efea081cc7ed9e60151448c620cec7815515e102
Julio Arias <jarias01@gmail.com>
2015-10-11 19:27:12 -0600

    Big cleanup to follow similar structure as linux-setup

--- mac-setup.sh
+++ mac-setup.sh
@@ -5,16 +5,8 @@ if [ ! -f /usr/local/bin/brew ]; then
 	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-#No password sudo
-sudo sed -i .bak -e 's/^# \(%wheel.*ALL=(ALL) NOPASSWD: ALL\)/\1/g' /etc/sudoers
-sudo dscl . append /Groups/wheel GroupMembership $USER
-
 #Install Ansible
 brew install ansible
 
 #Run Ansible
-PYTHONIOENCODING='utf-8' ansible-playbook desktop.yml
-
-#Return to normal password sudo
-sudo sed -i .bak -e 's/^\(%wheel.*ALL=(ALL) NOPASSWD: ALL\)/# \1/g' /etc/sudoers
-sudo dscl . delete /Groups/wheel GroupMembership $USER
+PYTHONIOENCODING='utf-8' ansible-playbook desktop.yml --ask-sudo-pass
--- roles/git/files/gitconfig.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-git config --global user.name "$(finger `whoami` | awk -F: '{ print $3 }' | head -n1 | sed 's/^ //')"
-git config --global user.email jarias01@gmail.com
